e8a6a33b0c9b8880bc2562c4a164f02e293461b8
Yan Robim <yanrobim@gmail.com>
2026-01-05 17:49:40 -0300

    Desenvolvendo filtro para remover/substituir linhas com nomes vázios (Ainda desenvolvendo).

---
+++ desafio_python/src/read_raw_file.py
@@ -0,0 +1,26 @@
+import csv
+
+def read_csv_file(path_csv_file: str):
+    csv_file = open(path_csv_file, mode='r+', newline='', encoding='utf-8')
+    data = csv.DictReader(csv_file, delimiter=",")     # Lendo o CSV importando o método (csv), uma lib própria e disponibilizada pelo python.
+                                                       # DictReader retorna nossos dados CSV em chave-valor, cada coluna sendo reconhecida como chave e os dados de colunas(individualmente) valor.
+                                                       # A variável delimiter é adicionada ao código para indicar o caractere que separa as colunas uma das outras no nosso CSV sendo a ","(Vírgula). 
+    return list(data)
+    
+
+read = read_csv_file(path_csv_file='./data/raw/funcionarios.csv')
+clean_data = []
+
+for linha in read:
+    linha_id = {coluna: dados for coluna, dados in linha.items() if not dados == ''}
+
+
+     
+    print(linha_id)
+            
+
+
+
+# with open("output.csv", "wb") as f:
+#     writer = csv.writer(f)            # Para salvar e garar relatório.
+#     writer.writerows(output_data)
