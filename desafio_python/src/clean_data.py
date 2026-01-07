eb4323a08dd9863d2a1128780ddcdb67cc37f281
Yan Robim <yanrobim@gmail.com>
2026-01-07 10:41:58 -0300

    Limpeza de dados do campo 'nome' vazio ou nomes com numeros.

--- desafio_python/src/clean_data.py
+++ desafio_python/src/clean_data.py
@@ -3,21 +3,23 @@ from read_raw_file import read_csv_file
 import re
 
 read = read_csv_file(path_csv_file='./data/raw/funcionarios.csv')
-clean_data = []
+clean_data = {}
 
 for linha in read:
-        if not linha['nome'] == '':         # Filtro para remover linhas que possuem nome = ''.
-                                            # No desafio, pedem que o relatorio_individual (o que iremos criar) somente possua registros válidos seguindo as Regras de Validação.
-            clean_data.append(linha)
+      if not linha['nome'] == '':         # Filtro para remover linhas que possuem nome = ''.
+                                          # No desafio, pedem que o relatorio_individual (o que iremos criar) 
+                                          # somente possua registros válidos seguindo as Regras de Validação.
+                  
+            nome_numero =re.sub(r'\d+', '', linha['nome'])  # re.sub faz a função de um replace, 
+                                                            # com um código regex encontra um número na string e 
+                                                            # substitui por '', uma string vázia, "apagando" o número da string.
             
-            encontrar_numero_string = re.findall(r'-?\d*\.?\d+', linha['nome'])
-            res = [float(number) if "." in number else int(number) for number in encontrar_numero_string]
-            for number in res:
-                  nome_numero = re.sub(r'\d+', '', linha['nome'])
+            linha.update({'nome': nome_numero.lstrip().rstrip()}) # Garantindo que os nomes que forem limpos 
+                                                                  # as strings não contenham espaços depois da limpeza, 
+                                                                  # com lstrip() caso o número fosse no começo do nome 
+                                                                  # e com rstrip() caso o número estivesse no fim.
 
-                  print(nome_numero)
 
-print(clean_data)
                     
 
                   
