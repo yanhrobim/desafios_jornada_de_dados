11013c03be327e40c24e501a196742b220c705c8
Yan Robim <yanrobim@gmail.com>
2026-01-07 17:27:58 -0300

    Adicionando etapa de limpeza de dados das colunas

--- desafio_python/src/main.py
+++ desafio_python/src/main.py
@@ -1,8 +1,10 @@
-from src.read_raw_file import read_csv_file
+from read_raw_file import read_csv_file
+from clean_data import clean_data
 
 def pipeline():
     funcionários_csv = './data/raw/funcionarios.csv'
     read_file = read_csv_file(funcionários_csv)
+    clean = clean_data(read_file)
 
 
 if __name__ == "__main__":
