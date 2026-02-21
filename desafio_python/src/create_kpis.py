ffe73762062e75973188ddf75df1061e6427f899
Yan <yanrobim@gmail.com>
2026-02-21 18:56:36 -0300

    KPI 'Quantidade de Funcionarios Por Area' Finalizada

--- desafio_python/src/create_kpis.py
+++ desafio_python/src/create_kpis.py
@@ -1,20 +1,26 @@
 from read_raw_file import read_csv_file
 import collections
+import json
 
 
-relatorio_individual = read_csv_file("./data/report/relatorio_individual.csv")
+def quantidade_funcionario_por_area(path_to_kpis_json: str):
+    relatorio_individual = read_csv_file("./data/report/relatorio_individual.csv")
 
-quantidade_funcionario_area = {}
-count = collections.Counter()
-for dados in relatorio_individual:
-    areas = dados
-
-    if dados["area"] == "TI":
-
+    quantidade_funcionario_area = {}
+    count = collections.Counter()
+    for dados in relatorio_individual:
 
         count[dados["area"]] += 1
-        quantidade_funcionario_area.update({"TI": count["TI"]})
 
+        quantidade_funcionario_area.update({"Quantidade de Funcionarios na Area de TI": count["TI"],
+                                            "Quantidade de Funcionarios na Area de Vendas": count["Vendas"],
+                                            "Quantidade de Funcionarios na Area do Financeiro": count["Financeiro"],
+                                            "Quantidade de Funcionarios na Area de Operacoes": count["Operações"],
+                                            "Quantidade de Funcionarios na Area do RH": count["RH"]})
+        
+    with open(path_to_kpis_json, 'w', encoding='utf-8') as kpis_json:
+        
+        json.dump(quantidade_funcionario_area, kpis_json, indent=4)
 
-print(quantidade_funcionario_area)
+quantidade_funcionario_por_area(path_to_kpis_json="./data/kpis/kpis.json")
 
