d088819c3fa4fe2f718e05a3639d4a96d0f60f4e
Yan <yanrobim@gmail.com>
2026-02-23 18:45:56 -0300

    KPI: Media de Salario por Area Finalizada.

--- desafio_python/src/create_kpis.py
+++ desafio_python/src/create_kpis.py
@@ -3,7 +3,9 @@ import collections
 import json
 
 
-def quantidade_funcionario_por_area(path_to_kpis_json: str):
+relatorio_individual = read_csv_file("./data/report/relatorio_individual.csv")
+
+def quantidade_funcionario_por_area():
     relatorio_individual = read_csv_file("./data/report/relatorio_individual.csv")
 
     quantidade_funcionario_area = {}
@@ -17,10 +19,67 @@ def quantidade_funcionario_por_area(path_to_kpis_json: str):
                                             "Quantidade de Funcionarios na Area do Financeiro": count["Financeiro"],
                                             "Quantidade de Funcionarios na Area de Operacoes": count["Operações"],
                                             "Quantidade de Funcionarios na Area do RH": count["RH"]})
-        
-    with open(path_to_kpis_json, 'w', encoding='utf-8') as kpis_json:
-        
-        json.dump(quantidade_funcionario_area, kpis_json, indent=4)
 
-quantidade_funcionario_por_area(path_to_kpis_json="./data/kpis/kpis.json")
+    return quantidade_funcionario_area
+
+def media_salario_por_area():
+
+    relatorio_individual = read_csv_file("./data/report/relatorio_individual.csv")
+
+    avg_salario_por_area = {}
+    salarios_ti = []
+    salarios_vendas = []
+    salarios_rh = []
+    salarios_operacoes = []
+    salarios_financeiro = []
+
+    for dados in relatorio_individual:
+
+        if dados['area'] == 'TI':
+            salarios_ti.append(float(dados['salario']))   # Precisamos de uma lista pois a função sum somente faz a soma de objetos iteráveis, 
+                                                    # como estamos em um loop de um Dict, os valores não eram passados de forma sequencial,
+                                                    # e sim somente o último valor encontrado da coluna específica.
+                                                    # Com a lista, o loop For adiciona todos os valores obtidos com append.
+
+            media_de_salarios_ti = sum(salarios_ti) / len(salarios_ti)
+            avg_salario_por_area.update({"Media do Salario na Area de TI": round(media_de_salarios_ti, 2)})
+        
+        if dados['area'] == 'Vendas':
+            salarios_vendas.append(float(dados['salario']))    
+
+            media_de_salarios_vendas = sum(salarios_vendas) / len(salarios_vendas)
+
+            avg_salario_por_area.update({"Media do Salario na Area de Vendas": round(media_de_salarios_vendas, 2)})
+
+        if dados['area'] == 'RH':
+                salarios_rh.append(float(dados['salario']))   
+
+                media_de_salarios_rh = sum(salarios_rh) / len(salarios_rh)
+
+                avg_salario_por_area.update({"Media do Salario na Area de RH": round(media_de_salarios_rh, 2)})
+
+        if dados['area'] == 'Operações':
+            salarios_operacoes.append(float(dados['salario']))    
+
+            media_de_salarios_operacoes = sum(salarios_operacoes) / len(salarios_operacoes)
+
+            avg_salario_por_area.update({f"Media do Salario na Area de Operacoes": round(media_de_salarios_operacoes, 2)})
+            # Para médias que resultam em Dízima Periódica, utilizo o Round para arrendodar o valor real.
+            # Com o objetivo de ter/fazer uma validação de dados.
+
+        if dados['area'] == 'Financeiro':
+            salarios_financeiro.append(float(dados['salario']))    
+
+            media_de_salarios_financeiro = sum(salarios_financeiro) / len(salarios_financeiro)
+
+            avg_salario_por_area.update({"Media do Salario na Area de Financeiro": round(media_de_salarios_financeiro, 2)})
+
+    return avg_salario_por_area
+
+
+
+
+
+
+
 
